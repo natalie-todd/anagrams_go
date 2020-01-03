bca3566982431e186052709a4dc0402d9a76b187
natalie-todd <natalie.todd15@gmail.com>
2020-01-03 10:07:35 -0700

    go get and import gorilla mux

--- main/main.go
+++ main/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -10,23 +12,24 @@ func home(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"message:", "get wrecked"}`))
+		w.Write([]byte(`{"message": "get called"}`))
 	case "POST":
 		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(`{"message:", "post it"}`))
+		w.Write([]byte(`{"message": "post called"}`))
 	case "PUT":
 		w.WriteHeader(http.StatusAccepted)
-		w.Write([]byte(`{"message:", "accept the put"}`))
+		w.Write([]byte(`{"message": "put called"}`))
 	case "DELETE":
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"message:", "delete"}`))
+		w.Write([]byte(`{"message": "delete called"}`))
 	default:
 		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte(`{"message:", "cant find it"}`))
+		w.Write([]byte(`{"message": "not found"}`))
 	}
 }
 
 func main() {
-	http.HandleFunc("/", home)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
