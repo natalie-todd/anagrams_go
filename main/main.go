12113c4e41e938b8e94105b3f791cf38960e996c
natalie-todd <natalie.todd15@gmail.com>
2020-01-03 14:12:24 -0700

    completed build-your-first-rest-api tutorial

--- main/main.go
+++ main/main.go
@@ -1,49 +1,77 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
-func get(w http.ResponseWriter, r *http.Request) {
+func get(w http.ResponseWriter, request *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"message": "get called"}`))
 }
 
-func post(w http.ResponseWriter, r *http.Request) {
+func post(w http.ResponseWriter, request *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(`{"message": "post called"}`))
 }
 
-func put(w http.ResponseWriter, r *http.Request) {
+func put(w http.ResponseWriter, request *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusAccepted)
 	w.Write([]byte(`{"message": "put called"}`))
 }
 
-func delete(w http.ResponseWriter, r *http.Request) {
+func delete(w http.ResponseWriter, request *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"message": "delete called"}`))
 }
 
-func notFound(w http.ResponseWriter, r *http.Request) {
+func params(w http.ResponseWriter, request *http.Request) {
+	pathParams := mux.Vars(request)
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusNotFound)
-	w.Write([]byte(`{"message": "not found"}`))
+
+	userID := -1
+	var err error
+	if val, ok := pathParams["userID"]; ok {
+		userID, err = strconv.Atoi(val)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(`{"message:" "need a number"}`))
+			return
+		}
+	}
+
+	commentID := -1
+	if val, ok := pathParams["commentID"]; ok {
+		commentID, err = strconv.Atoi(val)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(`{"message:" "need a number"}`))
+			return
+		}
+	}
+	query := request.URL.Query()
+	location := query.Get("location")
+
+	w.Write([]byte (fmt.Sprintf(`{"userID": %d, "commentID": %d, "location": %s}`, userID, commentID, location)))
 }
 
 func main() {
-	r := mux.NewRouter()
-	api := r.PathPrefix("/api/v1").Subrouter()
+	router := mux.NewRouter()
+	api := router.PathPrefix("/api/v1").Subrouter()
 	api.HandleFunc("", get).Methods(http.MethodGet)
 	api.HandleFunc("", post).Methods(http.MethodPost)
 	api.HandleFunc("", put).Methods(http.MethodPut)
 	api.HandleFunc("", delete).Methods(http.MethodDelete)
-	api.HandleFunc("", notFound)
-	log.Fatal(http.ListenAndServe(":8080", r))
+
+	api.HandleFunc("/user/{userID}/comment/{commentID}", params).Methods(http.MethodGet)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
